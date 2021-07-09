9495034b8138ca81ac9749e0065b0a1f05cbfb02
a-zankov <artzankoff@gmail.com>
2021-07-09 17:51:58 +0300

    Parametrization lesson

---
+++ Selenium_stepic/test_parametrization_lesson.py
@@ -0,0 +1,28 @@
+import pytest
+from selenium import webdriver
+import time
+import math
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(5)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+@pytest.mark.parametrize('url', ["236895", "236896", "236897", "236898", "236899", "236903", "236904", "236905"])
+def test_stepic_parametrs_lesson(browser, url):
+    link = f"https://stepik.org/lesson/{url}/step/1"
+    browser.get(link)
+    answer = str(math.log(int(time.time())))
+    input1 = browser.find_element_by_css_selector('[placeholder="Type your answer here..."]')
+    input1.send_keys(answer)
+    button1 = browser.find_element_by_css_selector('button.submit-submission')
+    button1.click()
+    otvet = browser.find_element_by_css_selector('pre.smart-hints__hint')
+    correct_otvet = otvet.text
+    assert correct_otvet == "Correct!", "Неправильный ответ"
