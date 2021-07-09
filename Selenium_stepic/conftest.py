31bd8c82921f44cc4526c3b82a359174c2d4305f
a-zankov <artzankoff@gmail.com>
2021-07-09 19:29:24 +0300

    Contest file browser option added

--- Selenium_stepic/conftest.py
+++ Selenium_stepic/conftest.py
@@ -2,6 +2,11 @@ import pytest
 from selenium import webdriver
 
 
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+
+
 @pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
